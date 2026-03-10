4a605ecbfdb6c6b1ca30c4e75b8a34a513033677
yeoni2026 <yeoni2026@gmail.com>
2026-03-10 15:35:01 +0900

    main파일 기초구현2

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 import pygame
+import generator
+import logic
 
 pygame.init()
 screen = pygame.display.set_mode((500, 800))
-clock = pygame.time.Clock()  # 추가
+clock = pygame.time.Clock()
 
+correct_answer = generator.function()
+A = False
 running = True
+
 while running:
-    for event in pygame.event.get():  # 추가
-        if event.type == pygame.QUIT:  # 추가
+    while not A:
+        answer = input("입력하세요 : ")
+        A = logic.function(answer)
+
+    print(logic.compare(answer,correct_answer))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
             running = False
+
     screen.fill((255, 255, 255))     
     pygame.display.flip()
     clock.tick(80)
